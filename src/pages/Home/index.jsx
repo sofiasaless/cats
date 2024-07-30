613870fc1dd64bedb8d213234e05d7b51a59ec06
sofiasales02 <sofiasaleswk@gmail.com>
2024-07-30 13:54:51 -0300

    home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,28 @@
 import Container from "../../components/Container";
 import Header from "../../components/Header";
+import gtx_logo from "../../../public/images/home-logoCat.png";
+import styles from './Home.module.css';
 
 export default function Home () {
     return (
         <>
             <Header/>
             <Container>
-                
+                {
+                    <section className={styles.section}>
+                        <div className={styles.texts}>
+                            <h1>Gatuxos <br /> Api</h1>
+                            <p>Site para fins de teste para API Spring. Cadastre um gatinho e o visuailize para testar!</p>
+                        </div>
+
+                        <div className={styles.image_content}>
+                            <img src={gtx_logo} alt="Logo inicial dos Gatuxos" />
+                            <div className={styles.shadow}>
+
+                            </div>
+                        </div>
+                    </section>
+                }
             </Container>
         </>
     );
